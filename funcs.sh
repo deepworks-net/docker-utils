604d7f73ada992e94239aad7677331ae77f582aa
Matt Buske <matt@deepworks.net>
2022-01-17 14:17:46 -0500

    Updated Funcs
    Update functions to work with docker hub

--- funcs.sh
+++ funcs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# DOCKER_DOMAIN = The domain to publish to. IE docker.myrepo.com
+# DOCKER_DOMAIN = The domain to publish to. IE docker.myrepo.com (if a private repository) (optional, defaults to offical docker hub)
 # IMAGE_PATH = The path where the image resides. IE mycompany
 # IMAGE_NAME = The name of the image. IE apache
 # BASEVER = The Major version of the image. IE 1
@@ -36,7 +36,15 @@
 #   docker tag apache:latest docker.myrepo.com/mycompany/apache:1
 #   docker tag apache:latest docker.myrepo.com/mycompany/apache:1.0
 #   docker tag apache:latest docker.myrepo.com/mycompany/apache:1.0.0
-#
+# 
+#  If the DOCKER_DOMAIN is empty, it will assume you are building for docker hub and will skip any tagging that includes the full repository name, ie:
+# 
+#   docker-compose build CentOS8
+#   
+#   docker tag apache:latest apache:1
+#   docker tag apache:latest apache:1.0
+#   docker tag apache:latest apache:1.0.0
+#   
 BUILD_IMAGE(){
 
     # Build image
@@ -48,17 +56,26 @@ BUILD_IMAGE(){
         _BASEVER=${BASEVER}; _BASEVER_WHOLE=${BASEVER_WHOLE}; _BASEVER_FULL=${BASEVER_FULL};
     fi 
 
+    _PRIV="NO"
+    if ! test -z "${DOCKER_DOMAIN}"; then _PRIV="YES"; fi
+
+    echo "$_PRIV"
+
     # make all tags
     docker tag ${IMAGE_NAME}:$2 ${IMAGE_NAME}:${_BASEVER}
     docker tag ${IMAGE_NAME}:$2 ${IMAGE_NAME}:${_BASEVER_WHOLE}
     docker tag ${IMAGE_NAME}:$2 ${IMAGE_NAME}:${_BASEVER_FULL}
-    docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$2
-    docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER}
-    docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_WHOLE}
-    docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_FULL}
+    if test "YES" = $_PRIV; then 
+        docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$2
+        docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER}
+        docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_WHOLE}
+        docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_FULL}
+    fi
     if ! test "latest" = $2; then
         docker tag ${IMAGE_NAME}:$2 ${IMAGE_NAME}:$2-latest;
-        docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$2-latest;
+        if test "YES" = $_PRIV; then 
+            docker tag ${IMAGE_NAME}:$2 ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$2-latest;
+        fi
     fi
 }
 
@@ -79,6 +96,13 @@ BUILD_IMAGE(){
 #   docker push docker.myrepo.com/mycompany/apache:1
 #   docker push docker.myrepo.com/mycompany/apache:1.0
 #   docker push docker.myrepo.com/mycompany/apache:1.0.0
+# 
+# If the DOCKER_DOMAIN is empty, it will assume you are publishing to docker hub and will omit the domain name, ie:
+#
+#   docker push mycompany/apache:latest
+#   docker push mycompany/apache:1
+#   docker push mycompany/apache:1.0
+#   docker push mycompany/apache:1.0.0
 #
 PUBLISH_IMAGE(){
 
@@ -87,13 +111,19 @@ PUBLISH_IMAGE(){
     if test "latest" = $1; then
         _BASEVER=${BASEVER}; _BASEVER_WHOLE=${BASEVER_WHOLE}; _BASEVER_FULL=${BASEVER_FULL};
     fi 
+     _PRIV="NO"
+    if ! test -z "${DOCKER_DOMAIN}"; then _PRIV="YES"; fi
+    _DOMAIN_PATH="";
+    if test "YES" = $_PRIV; then 
+        _DOMAIN_PATH="${DOCKER_DOMAIN}/${IMAGE_PATH}/";
+    fi 
 
-    docker push ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$1
-    docker push ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER}
-    docker push ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_WHOLE}
-    docker push ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:${_BASEVER_FULL}
-    if ! test "latest" = $2; then
-        docker push ${DOCKER_DOMAIN}/${IMAGE_PATH}/${IMAGE_NAME}:$1-latest;
+    docker push ${_DOMAIN_PATH}${IMAGE_NAME}:$1
+    docker push ${_DOMAIN_PATH}${IMAGE_NAME}:${_BASEVER}
+    docker push ${_DOMAIN_PATH}${IMAGE_NAME}:${_BASEVER_WHOLE}
+    docker push ${_DOMAIN_PATH}${IMAGE_NAME}:${_BASEVER_FULL}
+    if ! test "latest" = $1; then
+        docker push ${_DOMAIN_PATH}${IMAGE_NAME}:$1-latest;
     fi
 }
 
